304ec4cbbc2ea63495f6793550168131ecda834e
asadnadeem6321 <masadnadeem23@gmail.com>
2025-05-23 21:31:16 +0500

    Work with Models or Migrations File

--- myproject/myapp1/models.py
+++ myproject/myapp1/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    email = models.EmailField()
+    address = models.TextField()
+    phone_number = models.CharField(max_length=15)
+    date_of_birth = models.DateField()
+    enrollment_date = models.DateTimeField(auto_now_add=True)
+ 
