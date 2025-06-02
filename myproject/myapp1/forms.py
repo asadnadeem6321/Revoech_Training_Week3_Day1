4607bd929a3b1cf8f49724de30c3b7721144c9f7
asadnadeem6321 <masadnadeem23@gmail.com>
2025-06-03 01:41:11 +0500

    Forms.py added and define forms in it

---
+++ myproject/myapp1/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from .models import User
+from django.contrib.auth.forms import UserCreationForm
+
+class UserForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+
+    def __init__(self, *args, **kwargs):
+        super(UserForm, self).__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs.update({'class': 'form-control'})
+        self.fields['email'].widget.attrs.update({'class': 'form-control'})
+        self.fields['password1'].widget.attrs.update({'class': 'form-control'})
+        self.fields['password2'].widget.attrs.update({'class': 'form-control'})
